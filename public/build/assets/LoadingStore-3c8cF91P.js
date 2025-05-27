75a7d6809a693415ee5897a6fb1abcecd4c3bd2d
pramana yuda <pramanayuda772@gmail.com>
2025-05-27 20:33:34 +0700

    push

--- public/build/assets/LoadingStore-3c8cF91P.js
+++
@@ -1,1 +0,0 @@
-import{R as o,f as a}from"./app-Cb_B9rG2.js";const r=o("LoadingStore",()=>{const e=a(!1);return{isLoading:e,toggleLoading:()=>{e.value=!e.value}}});export{r as u};
--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -2,7 +2,7 @@
   <div class="not-found">
     <h1>404 - Page Not Found</h1>
     <p>The page you are looking for does not exist.</p>
-    <router-link to="/">Go Home</router-link>
+    <router-link to="/user/dashboard">Go Home</router-link>
   </div>
 </template>
 
