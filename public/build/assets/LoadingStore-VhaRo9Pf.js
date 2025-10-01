01df4f33dcf5453fe9832d5dcb744ed4fb378089
pramana yuda <pramanayuda772@gmail.com>
2025-10-01 15:13:11 +0700

    push parameter

--- public/build/assets/LoadingStore-VhaRo9Pf.js
+++
@@ -1,1 +0,0 @@
-import{R as o,e as a}from"./app-EcdR99ZA.js";const r=o("LoadingStore",()=>{const e=a(!1);return{isLoading:e,toggleLoading:()=>{e.value=!e.value}}});export{r as u};
---
+++ public/build/assets/LoadingStore-u0EL8OMM.js
@@ -0,0 +1,1 @@
+import{R as o,e as a}from"./app-CMjafPg1.js";const r=o("LoadingStore",()=>{const e=a(!1);return{isLoading:e,toggleLoading:()=>{e.value=!e.value}}});export{r as u};
